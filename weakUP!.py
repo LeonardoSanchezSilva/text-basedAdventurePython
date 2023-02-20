0eb572f4d6953485fd246b2dd089a044136a6819
Lonardo S. Silva <olaeusouofelipewelingson@gmail.com>
2023-02-20 15:00:34 -0300

    dando erro depois eu vejo

--- weakUP!.py
+++ weakUP!.py
@@ -1,7 +1,13 @@
 print('Você acorda em uma casa escura.\n A chuva castiga a velha madeira do casarão e o vidro da janela ao seu lado.')
 print('Sua perna dói. Você olha para ela?')
-perna = input('[S/N]')
-if(perna == 'S','s'):
-        print('sim')
-elif(perna == 'N', 'n'):
-        print('não')
+choice = input('[S/N] > ')
+
+if(choice == 'S' , 's'):
+    print('Você olha para sua perna. Na verdade, você olha para sua carne dilacerada. O sangue te faz ter vertigem.')   
+elif(choice == 'N' , 'n'):
+    print('Você decide não olhar para sua perna.')
+else:
+        print('Inválido. Você olha para sua perna?')
+
+print('Você não se lembra do que aconteceu.')
+
