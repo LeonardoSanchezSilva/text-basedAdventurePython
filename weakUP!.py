e52ef3a79834ae700195a9fcb8d02fbd81572e84
Lonardo S. Silva <olaeusouofelipewelingson@gmail.com>
2023-02-20 13:30:32 -0300

    Create weakUP!.py

---
+++ weakUP!.py
@@ -0,0 +1,7 @@
+print('Você acorda em uma casa escura.\n A chuva castiga a velha madeira do casarão e o vidro da janela ao seu lado.')
+print('Sua perna dói. Você olha para ela?')
+perna = input('[S/N]')
+if(perna == 'S','s'):
+        print('sim')
+elif(perna == 'N', 'n'):
+        print('não')
