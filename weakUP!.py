ae7310b10bb722a052c0b05596cd78372aedb837
Lonardo S. Silva <olaeusouofelipewelingson@gmail.com>
2023-02-20 15:38:52 -0300

    arrumei2

--- weakUP!.py
+++ weakUP!.py
@@ -1,13 +1,19 @@
 print('Você acorda em uma casa escura.\n A chuva castiga a velha madeira do casarão e o vidro da janela ao seu lado.')
 print('Sua perna dói. Você olha para ela?')
-choice = input('[S/N] > ')
 
-if choice in ['S', 's']:
-    print('Você olha para sua perna. Na verdade, você olha para sua carne dilacerada. O sangue te faz ter vertigem.')   
-elif choice in ['N', 'n']:
-    print('Você decide não olhar para sua perna.')
-else:
-        print('Opção invalida. Escolha entre [S] e [N]')
+choice = '> '
+valid_choice = False
+while not valid_choice:
+    choice = input('[S/N] > ')
+    if choice in ['S', 's']:
+        print('Você olha para sua perna. Na verdade, você olha para sua carne dilacerada. O sangue te faz ter vertigem.')
+        valid_choice = True
+    elif choice in ['N', 'n']:
+        print('Você decide não olhar para sua perna.')
+        valid_choice = True
+    else:
+        print('Opção inválida. Escolha entre [S] e [N]')
+        valid_choice = False
 
 print('Você não se lembra do que aconteceu.')
 
